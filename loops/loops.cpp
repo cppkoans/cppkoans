575db5069382bec9336662952b901d27714b3304
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-18 14:22:29 +0800

    Scope in loops

--- loops/loops.cpp
+++ loops/loops.cpp
@@ -9,5 +9,15 @@ int main() {
         second[i] = first[i];
         std::cout << second[i] << std::endl;
     }
+
+    // this allows us to access i outside the for-loop's scope
+    size_t i {};
+    // we can also not write i = 0 below as size_t i {} has
+    // initialized i as zero already
+    for (i = 0; i < 6; ++i) {
+        std::cout << second[i] << std::endl;
+    }
+    std::cout << i << std::endl;
+
     return 0;
 };
