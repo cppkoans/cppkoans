ad3a89799881724ab732e01863dd24793e08fecd
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-18 16:11:35 +0800

    Complications complications and erroneous results because of floating point additions

--- loops/loops.cpp
+++ loops/loops.cpp
@@ -21,12 +21,46 @@ int main() {
     std::cout << i << std::endl;
 
     // for-loop with floating point values
-    // setw from iomanip lines up our stdout prints vertically
+    // setw from iomanip lines up our stdout prints neatly, vertically
     const double pi {3.14159265};
     for (double radius {2.5}; radius <= 20.0; radius += 2.5) {
-        std::cout << "radius = " << std::setw(12) << radius
-            << " area = " << std::setw(12)
-            << pi * radius * radius << std::endl;
+        std::cout
+            << "radius = " << std::setw(12) << radius
+            << " area = " << std::setw(12) << pi * radius * radius
+            << std::endl;
+    }
+
+    const size_t perline {3};
+    size_t linecount {};
+    // complication when we do floating point additions
+    // our loop ends earlier than expected because 0.2 + 2.8 gives a value higher than 3.0!
+    std::cout << "First loop using increment steps of 0.2 " << std::endl;
+    for (double radius {0.2}; radius <= 3.0; radius += 0.2) {
+        std::cout
+            << std::fixed << std::setprecision(2)
+            << " radius = " << std::setw(5) << radius
+            << " area = " << std::setw(6) << pi * radius * radius;
+
+        if (perline == ++linecount) {
+            std::cout << std::endl;
+            linecount = 0;
+        }
+    }
+
+    std::cout << "\n" << std::endl;
+
+    std::cout << "Second loop using increment steps of .2 " << std::endl;
+    for (double radius {0.2}; radius <= 3.0; radius += .2) {
+        std::cout
+            << std::fixed << std::setprecision(2)
+            << " radius = " << std::setw(5) << radius
+            << " area = " << std::setw(6) << pi * radius * radius
+            << " delta to 3 = " << std::scientific << ((radius + 0.2) - 3.0) << std::endl;
+
+        if (perline == ++linecount) {
+            std::cout << std::endl;
+            linecount = 0;
+        }
     }
 
     return 0;
