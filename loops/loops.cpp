9060ea56a2b1f17166cace07135b7e7cdd13c190
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-18 15:35:30 +0800

    for loop with floating point values and setw vertical alignment

--- loops/loops.cpp
+++ loops/loops.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stddef.h>
+#include <iomanip>
 
 double first[6] {1.1, 2.8, 3.4, 3.7, 2.1, 2.3};
 double second[6] {};
@@ -18,6 +19,15 @@ int main() {
         std::cout << second[i] << std::endl;
     }
     std::cout << i << std::endl;
+
+    // for-loop with floating point values
+    // setw from iomanip lines up our stdout prints vertically
+    const double pi {3.14159265};
+    for (double radius {2.5}; radius <= 20.0; radius += 2.5) {
+        std::cout << "radius = " << std::setw(12) << radius
+            << " area = " << std::setw(12)
+            << pi * radius * radius << std::endl;
+    }
 
     return 0;
 };
