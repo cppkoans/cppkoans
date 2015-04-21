6782876693bfdf6c8bc439787072dbc2935224c3
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-21 16:40:57 +0800

    bitwise shift operations

---
+++ 03_datatypes/bitwise.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+unsigned short number {16387U};
+
+// bitwise shift operation
+// left shift operator << moves the bits by 2 towards the left
+// so, 0100000000000011 (16387)
+// becomes 0000000000001100 (which is 12)
+// right shift operator >> moving bits by 2 towards the right
+// so, 0100000000000011 (16387)
+// becomes 0001000000000000 (which is 4096)
+unsigned short result1 {static_cast<unsigned short>(number << 2)};
+unsigned short result2 {static_cast<unsigned short>(number >> 2)};
+
+int main()
+{
+    std::cout << number << std::endl;   // 16387
+    std::cout << result1 << std::endl;   // 12
+    std::cout << result2 << std::endl;   // 4096
+
+    return 0;
+}
