3754c73f3f50fcf6108e9d1c483bcda2c3e7ed20
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-21 22:17:50 +0800

    Using functions

---
+++ 08_functions/function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+
+double power(double x, int n)
+{
+    double result {1.0};
+    if (n >= 0)
+        for (int i {}; i < n; ++i)
+            result *= x;
+    else
+        for (int i {}; i < -n; ++i)
+            result /= x;
+    return result;
+}
+
+int main()
+{
+    // Calculate powers of 8 from -3 to +3
+    for (int i {-3}; i <= 3; ++i)
+        std::cout << std::setw(10) << power(8.0, i);
+
+    std::cout << std::endl;
+
+    return 0;
+}
