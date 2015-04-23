560908ad1a2948e50c1d8035e363b7dbaaf8e7e8
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-23 13:15:43 +0800

    Usage of default constructor

--- 11_classes/constructor.cpp
+++ 11_classes/constructor.cpp
@@ -16,6 +16,8 @@ public:
         width = widthValue;
         height = heightValue;
     }
+    Box() {}    // This allows us to use our default dimensions in private above if no
+                // lengthValue, widthValue and heightValue are given at initialization
     double volume()
     {
         return length * width * height;
@@ -27,6 +29,9 @@ int main()
     Box firstBox {80.0, 50.0, 40.0};
     double firstBoxVolume {firstBox.volume()};
     std::cout << "Volume of Box object is " << firstBoxVolume << std::endl;
+
+    Box secondBox;
+    std::cout << "Volume of the secondBox is " << secondBox.volume() << std::endl;
 
     return 0;
 }
