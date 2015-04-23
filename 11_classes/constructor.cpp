9c528decd6ece9f72992fbbca8c819fe4445f2d3
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-23 13:06:06 +0800

    classes, private and public and constructor

---
+++ 11_classes/constructor.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Box
+{
+private:
+    double length {1.0};
+    double width {1.0};
+    double height {1.0};
+
+public:
+    // Constructor
+    Box(double lengthValue, double widthValue, double heightValue)
+    {
+        std::cout << "Box constructor called." << std::endl;
+        length = lengthValue;
+        width = widthValue;
+        height = heightValue;
+    }
+    double volume()
+    {
+        return length * width * height;
+    }
+};
+
+int main()
+{
+    Box firstBox {80.0, 50.0, 40.0};
+    double firstBoxVolume {firstBox.volume()};
+    std::cout << "Volume of Box object is " << firstBoxVolume << std::endl;
+
+    return 0;
+}
