7361347a655b731e3a43d3deaa6c633aff9eef88
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-26 21:45:26 +0800

    function template overloading with a function template

--- 08_functions/functionoverload.cpp
+++ 08_functions/functionoverload.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <vector>
 
 template <typename T> T larger(T a, T b);   // function template prototype
 
-// function template overloading
+// function template overloading with a plain function (of the same name)
 long* larger(long* a, long* b);
+
+// function template overloading with function template
+template <typename T> T larger(const std::vector<T>& data);
 
 int main()
 {
@@ -21,6 +25,9 @@ int main()
 
     std::cout << "Larger of " << e << " and " << f << " is " << *larger(&e, &f) << std::endl;
 
+    std::vector<unsigned int> values { 2u, 3u, 5u, 7u, 11u, 13u, 17u, 19u, 6u };
+    std::cout << "Largest element is " << larger(values) << std::endl;
+
     return 0;
 }
 
@@ -30,11 +37,22 @@ T larger(T a, T b)
     return a > b ? a : b;
 }
 
-// function template overloading
+// function template overloading with function of the same name
 // when arguments are pointers, the comparison will be
 // done by value instead of by memory address
 // via this function overload
 long* larger(long* a, long* b)
 {
     return *a > *b ? a : b;
+}
+
+// function template overloading with another function template
+template <typename T>
+T larger(const std::vector<T>& data)
+{
+    T result {data[0]};
+    for (auto& value : data)
+        if (value > result) result = value;
+
+        return result;
 }
