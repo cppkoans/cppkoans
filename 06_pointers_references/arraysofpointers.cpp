3bb48050da151ac90b9f21e4f99fc9ce6a88d93f
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-29 09:47:47 +0800

    Arrays of pointers

---
+++ 06_pointers_references/arraysofpointers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    const char* pstars[] {
+        "Fatty Arbuckle", "Clara Bow",
+        "Lassie", "Slim Pickens",
+        "Boris Karloff", "Mae West",
+        "Oliver Hardy", "Greta Garbo"
+    };
+
+    size_t choice {};
+
+    std::cout << "Pick a lucky star! Enter a number between 1 and "
+              << sizeof(pstars) / sizeof(pstars[0]) << ": ";
+    std::cin >> choice;
+
+    if (choice >=1 && choice <= sizeof(pstars)/(sizeof pstars[0])) {
+        std::cout << "Your lucky star is " << pstars[choice - 1] << std::endl;
+    } else {
+        std::cout << "Sorry, you haven't got a lucky star." << std::endl;
+    }
+}
